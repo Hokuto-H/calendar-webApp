25eca89df3f7b25570452da592afc72063f0441c
Hokuto-H <ueuj38du3hd8@gmail.com>
2020-07-15 16:56:43 +0900

    リファクタリング

--- WEB-INF/src/model/Schedule.java
+++ WEB-INF/src/model/Schedule.java
@@ -7,15 +7,13 @@ public class Schedule {
 
     public void setSchedule(String weekday[], String time[], String subjects[]) {
         // * 曜日を代入
-        String str;
         /*
          * 月曜日の1限～6限を配列の0～5に代入 火曜日の1限～6限を配列の6～11に代入 水曜日の1限～6限を配列の12～17に代入
          * 木曜日の1限～6限を配列の18～23に代入 金曜日の1限～6限を配列の24～29に代入 土曜日の1限～6限を配列の30～35に代入
          * 日曜日の1限～6限を配列の36～41に代入
          */
         for (int i = 0; i < weekday.length; i++) {
-            str = weekday[i];
-            switch (str) {
+            switch (weekday[i]) {
                 case "月":
                     this.schedule[Integer.parseInt(time[i]) - 1] = subjects[i];
                     break;
